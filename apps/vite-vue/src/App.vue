f07867a880b42d0973974b8211caf5004645ac6b
tapan-7span <tapan@7span.com>
2024-02-20 11:05:16 +0530

    init commit

---
+++ apps/vite-vue/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <VueDatePicker v-model="date"></VueDatePicker>
+  </div>
+</template>
+<script setup>
+import { ref } from "vue";
+const date = ref(new Date());
+</script>
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
